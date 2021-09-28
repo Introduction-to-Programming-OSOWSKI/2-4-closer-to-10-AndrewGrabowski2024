2f25654e1c9f1d9802efb0698547b29b4fdda5d9
AndrewGrabowski2024 <819788@mystma.org>
2021-09-28 15:09:56 -0500

    3

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def close10(x,y):
     if abs(x-10) > abs(y-10):
         return y
     elif abs(x-10) == abs(y-10):
-        return "0"
+        return 0
     else :
         return x
 print(close10(11, 10))
