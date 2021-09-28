740684af0af7a6770e7440d5ea23cf58ff1da354
AndrewGrabowski2024 <819788@mystma.org>
2021-09-28 15:07:42 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 #WRITE YOUR CODE IN THIS FILE
+def close10(x,y):
+    if abs(x-10) > abs(y-10):
+        return y
+    elif abs(x-10) < abs(y-10):
+        return x
+    else :
+        return "0"
+print(close10(11, 10))
